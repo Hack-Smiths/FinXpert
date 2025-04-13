b8b019736a8e30796e69965de3c18fb6e80090e6
ramithachowdary <ramithachowdary433@gmail.com>
2025-04-13 13:18:50 +0530

    deleted

--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createUser, getUsers } = require("../controllers/userController");
-
-router.post("/", createUser);  // POST /api/users
-router.get("/", getUsers);     // GET /api/users
-
-module.exports = router;
